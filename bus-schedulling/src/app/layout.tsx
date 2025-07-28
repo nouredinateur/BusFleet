05dcbd0f77301473924b996b7b5afba02a0dfca2
nouredinateur <nafzaouinourdine@gmail.com>
2025-07-29 00:27:42 +0100

    feat: wrap children with UserProvider in layout
    Add UserProvider to manage user context across the application

--- bus-schedulling/src/app/layout.tsx
+++ bus-schedulling/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { QueryProvider } from "@/components/providers/query-provider";
+import { UserProvider } from "@/contexts/user-context";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>
-          {children}
+          <UserProvider>
+            {children}
+          </UserProvider>
         </QueryProvider>
       </body>
     </html>

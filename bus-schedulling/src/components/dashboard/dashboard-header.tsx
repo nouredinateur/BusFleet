f8bfe68923e5f506d00e69e7265ce0a1dd161b60
nouredinateur <nafzaouinourdine@gmail.com>
2025-07-28 22:17:23 +0100

    style(dashboard): update logout button styling for better visibility
    Improve button hover state by changing text color to accent and adding bold font weight for better visibility

--- bus-schedulling/src/components/dashboard/dashboard-header.tsx
+++ bus-schedulling/src/components/dashboard/dashboard-header.tsx
@@ -26,7 +26,7 @@ export function DashboardHeader() {
           </div>
           <Button
             variant="outline"
-            className="text-platinum-700 hover:text-black hover:border-error-500 font-inknut"
+            className="text-platinum-700 hover:text-accent font-bold hover:bg-black  font-inknut"
           >
             <LogOut className="w-4 h-4 mr-2" />
             Logout
